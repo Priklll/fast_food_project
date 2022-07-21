e6f0de6f46dacb2dd90f85403d51f4488105cc29
Дмитрий Приколота <priklll1989@gmail.com>
2022-07-21 22:05:48 +0300

    sign dis done

--- lib/page/login_sign.dart
+++ lib/page/login_sign.dart
@@ -87,7 +87,7 @@ class _BasicLoginSignPageState extends State<BasicLoginSignPage> {
                                       alignment: Alignment.bottomCenter,
                                       child: Text(
                                         'Sign-up',
-                                        style: TextStyle(fontFamily: 'Poppins', fontSize: 18, fontWeight: FontWeight.bold),
+                                        style: TextStyle(color: Colors.black, fontFamily: 'Poppins', fontSize: 18, fontWeight: FontWeight.bold),
                                       ),
                                     ),
                                   ),
@@ -108,7 +108,7 @@ class _BasicLoginSignPageState extends State<BasicLoginSignPage> {
               )),
           Expanded(
               flex: 6,
-              child: login ? Login() : Container()),
+              child: sign ? Sign() : Login()),
         ],
       ),
     );
@@ -262,6 +262,218 @@ class _BasicLoginSignPageState extends State<BasicLoginSignPage> {
                 ),
               ],),
           ),
+        ),
+        const Spacer(),
+      ],
+    );
+  }
+
+  Widget Sign () {
+    return Column(children: <Widget>[
+      const Spacer(),
+      Row(children: [
+        const Spacer(flex: 2,),
+        Text('Register', style: TextStyle(fontFamily: 'Nunito', fontSize: 36, fontWeight: FontWeight.bold, color: Color(0xffF8774A))),
+        const Spacer(),
+        /// Google ///
+        ///
+        NeumorphicButton(
+          onPressed: () {
+          },
+          style: NeumorphicStyle(
+            color: Colors.white,
+            depth: 3,
+            shape: NeumorphicShape.concave,
+            lightSource: LightSource.top,
+            boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(10)),
+          ),
+          padding: const EdgeInsets.all(12.0),
+          child: Container(
+            alignment: Alignment.center,
+            width: 25,
+            height: 25,
+            child: Image.asset('assets/LogSign/Google_logo.png', scale: 0.8,),
+          ),
+        ), const SizedBox(width: 16,),
+        /// Facebook ///
+        ///
+        NeumorphicButton(
+          onPressed: () {
+          },
+          style: NeumorphicStyle(
+            color: Colors.white,
+            depth: 3,
+            shape: NeumorphicShape.concave,
+            lightSource: LightSource.top,
+            boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(10)),
+          ),
+          padding: const EdgeInsets.all(12.0),
+          child: Container(
+            alignment: Alignment.center,
+            width: 25,
+            height: 25,
+            child: Image.asset('assets/LogSign/logos_facebook.png', scale: 0.8,),
+          ),
+        ), const Spacer(flex: 2,),
+      ],),  const Spacer(),
+      /// Name ///
+      ///
+      ///
+      Neumorphic(
+        // margin: EdgeInsets.only(left: 25, right: 25),
+        style: NeumorphicStyle(
+            depth: 3,
+            color: Colors.white,
+            lightSource: LightSource.top,
+            boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(10))),
+        child: Container(
+          width: 290,
+          height: 55,
+          child: TextFormField(
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.emailAddress,
+            // controller: _controllerEmail,
+            // onSubmitted: _changeEmail,
+            // onChanged: _changeEmail,
+            decoration: InputDecoration(
+              labelText: 'Full Name',
+              labelStyle: const TextStyle(fontSize: 16, color: Color(0xffA0A0A0), fontFamily: 'Nunito'),
+              fillColor: Colors.white,
+              filled: true,
+            ),
+            style: const TextStyle(fontSize: 16, color: Colors.black, fontFamily: 'Nunito'),
+          ),
+        ),
+      ), const Spacer(),
+      /// Mobile Number ///
+      ///
+      ///
+      Neumorphic(
+        // margin: EdgeInsets.only(left: 25, right: 25),
+        style: NeumorphicStyle(
+            depth: 3,
+            color: Colors.white,
+            lightSource: LightSource.top,
+            boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(10))),
+        child: Container(
+          width: 290,
+          height: 55,
+          child: TextFormField(
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.emailAddress,
+            // controller: _controllerEmail,
+            // onSubmitted: _changeEmail,
+            // onChanged: _changeEmail,
+            decoration: InputDecoration(
+              labelText: 'Mobile Number',
+              labelStyle: const TextStyle(fontSize: 16, color: Color(0xffA0A0A0), fontFamily: 'Nunito'),
+              fillColor: Colors.white,
+              filled: true,
+            ),
+            style: const TextStyle(fontSize: 16, color: Colors.black, fontFamily: 'Nunito'),
+          ),
+        ),
+      ), const Spacer(),
+      /// Password ///
+      ///
+      ///
+      Neumorphic(
+        // margin: EdgeInsets.only(left: 25, right: 25),
+        style: NeumorphicStyle(
+            depth: 3,
+            color: Colors.white,
+            lightSource: LightSource.top,
+            boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(10))),
+        child: Container(
+          width: 290,
+          height: 55,
+          child: TextFormField(
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.emailAddress,
+            // controller: _controllerEmail,
+            // onSubmitted: _changeEmail,
+            // onChanged: _changeEmail,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              labelStyle: const TextStyle(fontSize: 16, color: Color(0xffA0A0A0), fontFamily: 'Nunito'),
+              fillColor: Colors.white,
+              filled: true,
+            ),
+            style: const TextStyle(fontSize: 16, color: Colors.black, fontFamily: 'Nunito'),
+          ),
+        ),
+      ), const Spacer(),
+      /// Confirm Password ///
+      ///
+      ///
+      Neumorphic(
+        // margin: EdgeInsets.only(left: 25, right: 25),
+        style: NeumorphicStyle(
+            depth: 3,
+            color: Colors.white,
+            lightSource: LightSource.top,
+            boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(10))),
+        child: Container(
+          width: 290,
+          height: 55,
+          child: TextFormField(
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.emailAddress,
+            // controller: _controllerEmail,
+            // onSubmitted: _changeEmail,
+            // onChanged: _changeEmail,
+            decoration: InputDecoration(
+              labelText: 'Confirm Password',
+              labelStyle: const TextStyle(fontSize: 16, color: Color(0xffA0A0A0), fontFamily: 'Nunito'),
+              fillColor: Colors.white,
+              filled: true,
+            ),
+            style: const TextStyle(fontSize: 16, color: Colors.black, fontFamily: 'Nunito'),
+          ),
+        ),
+      ), const Spacer(),
+      /// Button Sign-Up ///
+      ///
+      ///
+      Row(children: <Widget>[
+        const Spacer(flex: 2,),
+        NeumorphicButton(
+          onPressed: () {},
+          style: NeumorphicStyle(
+            color: Color(0xffF8774A),
+            depth: 3,
+            shape: NeumorphicShape.concave,
+            lightSource: LightSource.top,
+            boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(30)),
+          ),
+          padding: const EdgeInsets.all(12.0),
+          child: Container(
+            alignment: Alignment.center,
+            width: 140,
+            height: 30,
+            child:  Text(
+                  'Sign-up',
+                  style: TextStyle(fontSize: 17, color: Colors.white, fontWeight: FontWeight.w600, fontFamily: 'Poppins'),
+
+              ),
+              ),
+            ),
+            const Spacer(),
+            Container(
+              height: 50,
+              width: 130,
+              child: RichText(
+                maxLines: 4,
+                text: TextSpan(
+                 style: TextStyle(fontSize: 16, color: Color.fromRGBO(179, 179, 179, 1), fontFamily: 'Nunito'),
+                  children: [
+                    TextSpan(text: 'Already a Member?', style: TextStyle(fontWeight: FontWeight.w400)),
+                    TextSpan(text: ' Login', style: TextStyle(fontWeight: FontWeight.w700)),
+                  ]
+                ),
+              ),
+            ), const Spacer(flex: 2,),
+          ],
         ),
         const Spacer(),
       ],
